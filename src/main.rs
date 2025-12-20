62321a88e1eb0b3f367f0af80fcffdd142e3b7eb
pk-kher <pradipkher3@gmail.com>
2025-12-20 19:12:58 +0530

    implement decode blob object try 1

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use clap::{Parser, Subcommand};
+use flate2::read::ZlibDecoder;
 use std::fs;
+use std::io::{self, Read, Write};
+
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
@@ -9,18 +12,41 @@ struct Args {
 
 #[derive(Subcommand, Debug)]
 enum Commands {
-    init,
+    Init,
+    CatFile {
+        /// lists test values
+        #[arg(short = 'p')]
+        pretty_print: bool,
+        object_hash: String,
+    },
 }
-fn main() {
+fn main() -> io::Result<()> {
     let args = Args::parse();
     println!("{:?}", args);
     match args.command {
-        Commands::init => {
+        Commands::Init => {
             fs::create_dir(".git").unwrap();
             fs::create_dir(".git/objects").unwrap();
             fs::create_dir(".git/refs").unwrap();
             fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
             println!("Initialized git directory")
         }
+        Commands::CatFile {
+            pretty_print,
+            object_hash,
+        } => {
+            let f = fs::read(format!(
+                ".git/objects/{}/{}",
+                &object_hash[..2],
+                &object_hash[2..],
+            ))
+            .unwrap();
+            let mut z = ZlibDecoder::new(&f[..]);
+            let mut s = String::new();
+            z.read_to_string(&mut s)?;
+            print!("{}", s);
+            io::stdout().flush().expect("Failed to flush stdout");
+        }
     }
+    Ok(())
 }
