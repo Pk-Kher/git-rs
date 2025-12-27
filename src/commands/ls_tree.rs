f5099852b1c066a18ff57a67268f5589bd0c8f09
pk-kher <pradipkher3@gmail.com>
2025-12-27 12:12:58 +0530

    make reader separate

--- src/commands/ls_tree.rs
+++ src/commands/ls_tree.rs
@@ -1,55 +1,22 @@
-use anyhow::Context;
-use flate2::read::ZlibDecoder;
-use std::ffi::CStr;
-use std::fs;
+use std::io::Read;
 use std::io::prelude::*;
-use std::io::{BufReader, Read};
 
-use crate::objects::Kind;
+use crate::objects::{self, Kind};
 
 pub(crate) fn invoke(name_only: bool, tree_object: String) -> anyhow::Result<()> {
     // print!("{}, {:?} ", name_only, tree_object);
-    let file = fs::File::open(format!(
-        ".git/objects/{}/{}",
-        &tree_object[..2],
-        &tree_object[2..]
-    ))
-    .with_context(|| format!("Failed to open the file:{:?}", &tree_object))?;
-    let z = ZlibDecoder::new(file);
-    let mut z = BufReader::new(z);
-    let mut buf = Vec::new();
-    z.read_until(0, &mut buf)
-        .context("Reading header from .git/object")?;
-    let header = CStr::from_bytes_with_nul(&buf[..])
-        .expect("know there is exactly one nul, and it's at the end");
-    let header = header
-        .to_str()
-        .context(".git/objects file header isn't valid UTF-8")?;
-    let Some((kind, size)) = header.split_once(' ') else {
-        anyhow::bail!(".git/objects file header did not start with a known type: '{header}'");
-    };
-    let kind = match kind {
-        "tree" => Kind::Tree,
-        _ => anyhow::bail!("we do not yet know how to print a '{kind}'"),
-    };
-    let size = size
-        .parse::<usize>()
-        .context("Failed to parse size from &str to u64")?;
-    // let stdout = std::io::stdout();
-    // let mut stdout = stdout.lock();
-    // std::io::copy(&mut z, &mut stdout)?;
-    match kind {
+    let mut object = objects::Object::read(&tree_object)?;
+    match object.kind {
         Kind::Tree => {
-            let mut read_bytes: usize = 0;
+            let mut read_bytes: u64 = 0;
             let mut mode = Vec::new();
             let mut file_name = Vec::new();
             let mut hash = [0; 20];
-            while size > read_bytes {
+            while object.expected_size > read_bytes {
                 mode.clear();
                 file_name.clear();
-
-                read_bytes += z.read_until(b' ', &mut mode)?;
-                read_bytes += z.read_until(0, &mut file_name)?;
+                read_bytes += object.reader.read_until(b' ', &mut mode)? as u64;
+                read_bytes += object.reader.read_until(0, &mut file_name)? as u64;
 
                 if let Some(&b' ') = mode.last() {
                     mode.pop();
@@ -57,14 +24,14 @@ pub(crate) fn invoke(name_only: bool, tree_object: String) -> anyhow::Result<()>
                 if let Some(&0) = file_name.last() {
                     file_name.pop();
                 }
-                z.read_exact(&mut hash)?;
+                object.reader.read_exact(&mut hash)?;
                 read_bytes += 20;
 
                 if !name_only {
                     print!(
                         "{:0>6} {} ",
                         format!("{}", std::str::from_utf8(&mode)?),
-                        Kind::from_mode(&mode)?.to_str(),
+                        Kind::from_mode(&mode)?,
                     );
                     for byte in &hash {
                         print!("{:02x}", byte);
@@ -74,7 +41,7 @@ pub(crate) fn invoke(name_only: bool, tree_object: String) -> anyhow::Result<()>
                 println!("{}", std::str::from_utf8(&file_name)?);
             }
         }
-        _ => anyhow::bail!("we do not yet know how to print a '{:?}'", kind),
+        _ => anyhow::bail!("we do not yet know how to print a '{:?}'", &object.kind),
     }
     Ok(())
 }
--- src/objects.rs
+++ src/objects.rs
@@ -1,7 +1,17 @@
-#[derive(Debug)]
+use anyhow::Context;
+use core::fmt;
+use flate2::read::ZlibDecoder;
+use std::{
+    ffi::CStr,
+    fs,
+    io::{BufRead, BufReader, Read},
+};
+
+#[derive(Debug, PartialEq, Eq)]
 pub(crate) enum Kind {
     Blob,
     Tree,
+    Commit,
 }
 impl Kind {
     pub(crate) fn from_mode(mode: &[u8]) -> Result<Self, anyhow::Error> {
@@ -11,11 +21,55 @@ impl Kind {
             _ => anyhow::bail!("unknown mode:{:?}", std::str::from_utf8(mode)),
         }
     }
-
-    pub(crate) fn to_str(&self) -> &str {
+}
+impl fmt::Display for Kind {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Kind::Blob => "blob",
-            Kind::Tree => "tree",
+            Kind::Blob => write!(f, "blob"),
+            Kind::Tree => write!(f, "tree"),
+            Kind::Commit => write!(f, "commit"),
         }
+    }
+}
+
+pub(crate) struct Object<R> {
+    pub(crate) kind: Kind,
+    pub(crate) expected_size: u64,
+    pub(crate) reader: R,
+}
+
+impl Object<()> {
+    pub(crate) fn read(hash: &str) -> anyhow::Result<Object<impl BufRead>> {
+        let f = fs::File::open(format!(".git/objects/{}/{}", &hash[..2], &hash[2..],))
+            .context("File does't exits")?;
+
+        let z = ZlibDecoder::new(f);
+        let mut z = BufReader::new(z);
+        let mut buf = Vec::new();
+        z.read_until(0, &mut buf)
+            .context("Reading header from .git/object")?;
+
+        let header = CStr::from_bytes_with_nul(&buf)
+            .expect("know there is exactly one nul, and it's at the end");
+        let header = header
+            .to_str()
+            .context(".git/objects file header isn't valid UTF-8")?;
+        let Some((kind, size)) = header.split_once(' ') else {
+            anyhow::bail!(".git/objects file header did not start with a known type: '{header}'");
+        };
+
+        let kind = match kind {
+            "blob" => Kind::Blob,
+            "tree" => Kind::Tree,
+            "commint" => Kind::Commit,
+            _ => anyhow::bail!("we do not yet know how to print a '{kind}'"),
+        };
+        let size = size.parse::<u64>().context("file size is not valid")?;
+        let z = z.take(size);
+        Ok(Object {
+            kind,
+            expected_size: size,
+            reader: z,
+        })
     }
 }
